947bd54434cba6de1f653d3b8c8cf6444d7a1b44
admodev <adolfomoyano@exegesisba.com>
2021-12-04 08:39:19 -0300

    feat(component, package): add home icon to home component add commitizen package

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Wayni
+# Wayni Movil
 
-Webapp
+## Webapp desarrollada en React
+
+Para realizar un commit ejecutar:
+
+`npm run commit`
+
+o si usas yarn:
+
+`yarn run commit`
+
+o simplemente:
+
+`git cz`
--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -6,6 +6,17 @@ import './styles.css';
 
 const Home = () => (
   <div className="container">
+    <div className="icon-home">
+      <div className="home-body" />
+      <div className="home-roof-cut-left" />
+      <div className="home-roof-cut-right" />
+      <div className="home-door" />
+      <div className="home-chimney" />
+      <div className="home-roof-container">
+        <div className="home-roof-left" />
+        <div className="home-roof-right" />
+      </div>
+    </div>
     <Link className="title" to="/movements">
       Movimientos
     </Link>
